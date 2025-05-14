23f27257825a47aa7062e9ec5a5b308476fa872d
ittcl999 <it.tcl999@gmail.com>
2025-05-14 15:48:57 +0700

    Create api.js

---
+++ assets/js/api.js
@@ -0,0 +1,34 @@
+// เพิ่ม: URL ของ GAS Web App
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbym5zxW382G2enqHVpsEkltXSCeaWEXdWmUqpz11Wxfi2pxp8Pg2SP9RmCbfDLtPU6T/exec';
+
+/**
+ * เพิ่ม: ส่งข้อมูลฟอร์มคำขอเข้าพื้นที่
+ * @param {Object} data
+ */
+async function submitForm(data) {
+  // TODO: implement fetch POST
+}
+
+/**
+ * เพิ่ม: ตรวจสอบ CID ของผู้ร่วมงาน
+ * @param {string} cid
+ */
+async function checkWorkerCID(cid) {
+  // TODO: implement fetch GET
+}
+
+/**
+ * เพิ่ม: ดึงสถานะคำขอ
+ * @param {string} svrId
+ */
+async function getStatus(svrId) {
+  // TODO: implement fetch GET
+}
+
+/**
+ * เพิ่ม: บันทึกผลอนุมัติ (Approve/Reject)
+ * @param {Object} payload
+ */
+async function approveRequest(payload) {
+  // TODO: implement fetch POST
+}
